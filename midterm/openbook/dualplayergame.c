f716d1c4813d01bf28463286cc2577f2ca49f246
therohilpal <rohil.pal@iiitb.org>
2019-05-06 16:09:25 +0530

    Updated dualplayergame.c

--- midterm/openbook/dualplayergame.c
+++ midterm/openbook/dualplayergame.c
@@ -8,19 +8,26 @@
 
 int diceVal;
 sem_t locks[2];
+int snakes[7] = {5, 10, 15, 20, 25, 30, 35};
 
 void *play(void* player_lock){
 
+	int choice;
 	sem_wait(player_lock);
 	
 	// generate a random number between 1 to 6
 	diceVal = (rand() % 6) + 1;
+
+	choice = (rand() % 8) + 1; // 8 corresponds to 7 snakes + 1 no snake in the game
+
+	if (choice < 8)
+		diceVal -= snakes[choice-1];
 }
 
 int main(){
 	pthread_t p[2];
 	struct timeval stop, start;
-
+	srand(time(0)); 
 	int score[2] = {0, 0};
 
 	gettimeofday(&start, NULL);
@@ -38,18 +45,18 @@ int main(){
 
 		printf("Player 1: %d\t Player 2: %d\n", score[0], score[1]);
 
-	}while((score[0] < 5000) && (score[1] < 5000));
+	}while((score[0] < 100) && (score[1] < 100));
 
 	gettimeofday(&stop, NULL);
 
-	if (score[0]>5000)
+	if (score[0]>=100)
 		printf("Winner is Player 1 with score %d\n", score[0]);
 	
-	if (score[1]>5000)
+	if (score[1]>=100)
 		printf("Winner is Player 2 with score %d\n", score[1]);
 
 
-	printf("Total time (seconds): %lu\n", stop.tv_usec - start.tv_usec);
+	printf("Total time (microseconds): %lu\n", stop.tv_usec - start.tv_usec);
 
 	return 0;
 }
